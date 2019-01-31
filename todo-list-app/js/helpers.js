efa1958a49c4a5d504cb4e4efe875f4bbe4eb33a
ShannAlbane <45179917+ShannAlbane@users.noreply.github.com>
2019-01-31 14:37:59 +0100

    Ajout de commentaires sous forme de Javadoc

--- todo-list-app/js/helpers.js
+++ todo-list-app/js/helpers.js
@@ -2,7 +2,12 @@
 (function (window) {
 	'use strict';
 
-	// Get element(s) by CSS selector:
+	/**
+	 * Get element(s) by CSS selector
+	 *  
+	 * @param {string} selector The CSS selector
+	 * @param {object} scope The scope tested, or by default the entire document
+	 */ 
 	window.qs = function (selector, scope) {
 		return (scope || document).querySelector(selector);
 	};
@@ -10,14 +15,28 @@
 		return (scope || document).querySelectorAll(selector);
 	};
 
-	// addEventListener wrapper:
+	/**
+	 * Wrap a event listener to an existing element 
+	 *  
+	 * @param {object} target The element targeted
+	 * @param {boolean} type Blur or Focus
+	 * @param {function} callback The callback of the function
+	 * @param {object} useCapture useCapture is composed by the type (blur or focus)
+	 */ 
 	window.$on = function (target, type, callback, useCapture) {
 		target.addEventListener(type, callback, !!useCapture);
 	};
 
-	// Attach a handler to event for all elements that match the selector,
-	// now or in the future, based on a root element
-	window.$delegate = function (target, selector, type, handler) {
+	/** 
+	 * Attach a handler to event for all elements that match the selector,
+	 * now or in the future, based on a root element
+	 * 
+	 * @param {object} target The element targeted
+	 * @param {string} selector The CSS selector to filter the element
+	 * @param {boolean} type Blur or Focus 
+	 * @param {function} handler Callback executed (handler to attach) if the condition is verified	
+	 * 
+	 */ 	window.$delegate = function (target, selector, type, handler) {
 		function dispatchEvent(event) {
 			var targetElement = event.target;
 			var potentialElements = window.qsa(selector, target);
@@ -34,8 +53,16 @@
 		window.$on(target, type, dispatchEvent, useCapture);
 	};
 
-	// Find the element's parent with the given tag name:
-	// $parent(qs('a'), 'div');
+	/**
+	 * Find the element's parent with the given tag name:
+	 * @param {object} element The element targeted
+	 * @param {string} tagName The HTML tag to filter 
+	 * 
+	 * @example
+	 * $parent(qs('a'), 'div');
+	 * 	//will returns the <div> who contains <a> element 
+	 */ 
+	// 
 	window.$parent = function (element, tagName) {
 		if (!element.parentNode) {
 			return;
@@ -46,7 +73,12 @@
 		return window.$parent(element.parentNode, tagName);
 	};
 
-	// Allow for looping on nodes by chaining:
-	// qsa('.foo').forEach(function () {})
+	/**
+	 * Allow for looping on nodes by chaining
+	 * 
+	 * @example
+	 * qsa('.foo').forEach(function () {})
+	 * //will returns all elements with the HTML class 'foo'
+	 */ 
 	NodeList.prototype.forEach = Array.prototype.forEach;
 })(window);
